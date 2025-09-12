5b22be8bd1c8740da0d5e97e0e2efd9057f92993
aurielyn <aurielyn.dev@gmail.com>
2025-09-12 19:02:12 +0530

    im dumb

--- src/main/kotlin/meowing/zen/features/slayers/carrying/CarryCounter.kt
+++ src/main/kotlin/meowing/zen/features/slayers/carrying/CarryCounter.kt
@@ -327,8 +327,6 @@ object CarryCounter : Feature("carrycounter") {
             bossTimes.add(startTime.since.millis)
             cleanup()
 
-            if (carrycountsend) ChatUtils.command("/pc $name: $count/$total")
-
             if (++count >= total) {
                 complete()
                 if (carrywebhook.isEmpty()) return
@@ -368,6 +366,8 @@ object CarryCounter : Feature("carrycounter") {
                     onError = { LOGGER.warn("Carry-Webhook onKill POST failed: ${it.message}") }
                 )
             }
+
+            if (carrycountsend) ChatUtils.command("/pc $name: $count/$total")
         }
 
         fun reset() {

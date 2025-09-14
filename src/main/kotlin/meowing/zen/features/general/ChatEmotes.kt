68e240e06939edf02e171ef23f377c6750238951
MrFast <mrfastkrunker@gmail.com>
2025-09-14 14:41:42 -0400

    Add ChatEmotes | Increase cooldown for WorldEvent.Change

---
+++ src/main/kotlin/meowing/zen/features/general/ChatEmotes.kt
@@ -0,0 +1,71 @@
+package meowing.zen.features.general
+
+import meowing.zen.Zen
+import meowing.zen.config.ui.ConfigUI
+import meowing.zen.config.ui.types.ConfigElement
+import meowing.zen.config.ui.types.ElementType
+import meowing.zen.events.ChatEvent
+import meowing.zen.features.Feature
+import meowing.zen.utils.ChatUtils
+
+@Zen.Module
+object ChatEmotes : Feature("chatemotes") {
+    val HYPIXEL_EMOTES = mapOf(
+        "<3" to "❤",
+        ":star:" to "✮",
+        ":yes:" to "✔",
+        ":no:" to "✖",
+        ":java:" to "☕",
+        ":arrow:" to "➜",
+        ":shrug:" to "¯\\_(ツ)_/¯",
+        ":tableflip:" to "(╯°□°）╯︵ ┻━┻",
+        "o/" to "( ﾟ◡ﾟ)/",
+        ":123:" to "123",
+        ":totem:" to "◎_◎",
+        ":typing:" to "✎...",
+        ":maths:" to "√(π+x)=L",
+        ":snail:" to "@'-'",
+        ":thinking:" to "(0.o?)",
+        ":gimme:" to "༼つ ◕_◕ ༽つ",
+        ":wizard:" to "('-')⊃━☆ﾟ.*･｡ﾟ",
+        ":pvp:" to "⚔",
+        ":peace:" to "✌",
+        ":oof:" to "OOF",
+        ":puffer:" to "<('O')>",
+        ":snow:" to "☃",
+        ":dog:" to "(ᵔᴥᵔ)",
+        ":sloth:" to "( ⬩ ⊝ ⬩ )",
+        ":dab:" to "<o/",
+        ":cat:" to "ᓚᘏᗢ",
+    )
+
+    override fun addConfig(configUI: ConfigUI): ConfigUI {
+        return configUI
+            .addElement("General", "Chat Emotes", ConfigElement(
+                "chatemotes",
+                "Automatically replace emote codes with Unicode symbols in chat messages",
+                ElementType.Switch(true)
+            ), true)
+            .addElement("General", "Chat Emotes", "", ConfigElement(
+                "",
+                null,
+                ElementType.TextParagraph("Automatically replace emote codes with Unicode symbols in chat messages, example: <3 becomes ❤, use /emotes to see all supported emotes.")
+            ))
+
+    }
+
+    override fun initialize() {
+        register<ChatEvent.Send> { event ->
+            var message = event.message
+
+            HYPIXEL_EMOTES.forEach { (code, symbol) ->
+                message = message.replace(code, symbol)
+            }
+
+            if (message != event.message) {
+                event.cancel()
+                ChatUtils.chat(message)
+            }
+        }
+    }
+}
